e171d4f1deb023bfdc2e898503fd560138180a86
DestinyX2-Home <crvenegas17@alumnos.utalca.cl>
2021-11-11 19:02:25 -0300

    Recipes
    test Scriptable Object and Script

---
+++ Nodes_Project/Assets/Nodes/Scripts/Recipe.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[CreateAssetMenu]
+public class Recipe : ScriptableObject
+{
+    [SerializeField] Ingredient[] inputs;
+    [SerializeField] Ingredient[] outputs;
+
+    public Ingredient[] GetOutputs()
+    {
+        return outputs;
+    }
+
+    public Ingredient[] GetInputs()
+    {
+        return inputs;
+    }
+
+    public bool HasIngredients(Ingredient[] listToCheck)
+    {
+        foreach(Ingredient ingredient in inputs)
+        {
+            if(!listToCheck.Contains(ingredient))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
