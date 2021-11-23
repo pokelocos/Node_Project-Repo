43944959d6ab83b86d8badb74d49fca48894664a
pokelocos <nare.rare.fire@gmail.com>
2021-11-23 16:42:53 -0300

    hold button

---
+++ Nodes_Project/Assets/HoldButton.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class HoldButton : MonoBehaviour
+{
+    public UnityEvent evnt;
+
+    public float maxTime;
+    private float currentTime = 0f;
+
+    public Image img;
+
+    private bool boleano;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(boleano)
+        {
+            currentTime += Time.deltaTime;
+            img.fillAmount = currentTime / maxTime;
+
+            if (currentTime / maxTime >= 1)
+            {
+                evnt?.Invoke();
+                ResetTime();
+            }
+        }
+
+    }
+
+    public void StartTime()
+    {
+        boleano = true;
+    }
+
+    public void EndTime()
+    {
+        boleano = false;
+        ResetTime();
+    }
+
+    public void ResetTime()
+    {
+        currentTime = 0;
+        img.fillAmount = 0;
+    }
+}
