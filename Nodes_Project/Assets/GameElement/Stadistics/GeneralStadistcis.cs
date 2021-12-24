eb4bc7e12ede7ccc9e4505c72375091ece987087
pokelocos <nare.rare.fire@gmail.com>
2021-12-24 13:20:10 -0300

    main menu changes

--- Nodes_Project/Assets/GameElement/Stadistics/GeneralStadistcis.cs
+++ Nodes_Project/Assets/GameElement/Stadistics/GeneralStadistcis.cs
@@ -27,8 +27,11 @@ public class GeneralStadistcis
 
 
     public int gamesPlayed = 0;
-    public int gameWin = 0;
+    public int gamesWin = 0;
     public int totalMoneyEarned = 0;
+    public int totalMoneySpend = 0;
+    public int totalCiclesPlay = 0;
     public Dictionary<string, int> amountOfNodesPurchased = new Dictionary<string, int>();
+    public Dictionary<string, int> amountOfProductGenerated = new Dictionary<string, int>();
 
 }
